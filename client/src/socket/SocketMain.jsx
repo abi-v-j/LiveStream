f6e6b52332c058aa01c24e17e557b0ed5f2ed821
abi-v-j <abijoy611@gmail.com>
2024-11-04 15:16:56 +0530

    new

--- client/src/socket/SocketMain.jsx
+++ client/src/socket/SocketMain.jsx
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 // export const socket = io("http://localhost:5000/");
 export const socket = io("https://live-stream-server-pearl.vercel.app", {
-    transports: ["websocket", "polling"]
-  });
+    transports: ["websocket"],
+});
+
   
