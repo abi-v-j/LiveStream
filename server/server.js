6dd3b0e51ab6b726a29c9f45a8d550de6eebc19f
abi-v-j <abijoy611@gmail.com>
2024-11-04 13:32:18 +0530

    new

--- server/server.js
+++ server/server.js
@@ -1,1 +1,31 @@
-console.log('hi');
+const express = require("express"); 
+const app = express(); 
+const cors = require("cors");
+const bodyParser = require("body-parser"); 
+const port = 5000; 
+const dotenv = require('dotenv');
+dotenv.config
+
+
+
+
+
+
+
+
+
+//use express static folder
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+app.listen(port, () => {
+    try {
+        console.log(`Server is running ${port}`);
+
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+});
