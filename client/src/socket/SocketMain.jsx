7e726df067e846526f7b423b75cabb29bf005a70
abi-v-j <abijoy611@gmail.com>
2024-11-04 15:25:17 +0530

    new

--- client/src/socket/SocketMain.jsx
+++ client/src/socket/SocketMain.jsx
@@ -1,6 +1,14 @@
 import { io } from "socket.io-client";
 
 // export const socket = io("http://localhost:5000/");
-export const socket = io("https://live-stream-server-pearl.vercel.app/");
+// export const socket = io("https://live-stream-server-pearl.vercel.app/");
 
+export const socket = io("https://live-stream-server-pearl.vercel.app", {
+    transports: ["websocket"],
+    reconnection: true,
+    reconnectionAttempts: 5,
+    timeout: 20000
+  });
+
+  
   
